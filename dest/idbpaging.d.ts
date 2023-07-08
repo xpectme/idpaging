93faa4c4aeb7a1dfc3a3fdc6a6e164a491128808
Mario Stöcklein <mstoecklein@xp.live>
2023-07-08 22:57:16 +0200

    bundle for v1.1.0

--- dest/idbpaging.d.ts
+++ dest/idbpaging.d.ts
@@ -1,4 +1,11 @@
-export interface IDBPagingCalculatePageNumberOptions {
+export interface IDBPagingIndexOptions {
+    indexName?: string;
+    indexKey?: IDBValidKey;
+}
+export interface IDBPagingPageSizeOptions extends IDBPagingIndexOptions {
+    startPage?: number;
+}
+export interface IDBPagingCommandOptions extends IDBPagingIndexOptions {
     noCheck?: boolean;
     circularPaging?: boolean;
 }
@@ -12,14 +19,14 @@ export declare class IDBPaging<T> {
     totalPages: number;
     list: T[];
     constructor(db: IDBDatabase, storeName: string, pageSize?: number);
-    setPageSize(pageSize: number, startPageNumber?: number): Promise<void>;
-    calculateLastPageNumber(): Promise<number>;
-    calculatePageNumber(pageNumber: number | string, options?: IDBPagingCalculatePageNumberOptions): Promise<void>;
-    go(pageNumber: number, options?: IDBPagingCalculatePageNumberOptions): Promise<T[]>;
-    next(pages?: number, circularPaging?: boolean): Promise<T[]>;
-    prev(pages?: number, circularPaging?: boolean): Promise<T[]>;
-    first(): Promise<T[]>;
-    last(): Promise<T[]>;
-    count(): Promise<number>;
+    setPageSize(pageSize: number, options?: number | IDBPagingPageSizeOptions): Promise<void>;
+    calculateLastPageNumber(options?: IDBPagingIndexOptions): Promise<number>;
+    calculatePageNumber(pageNumber: number | string, options?: IDBPagingCommandOptions): Promise<void>;
+    go(pageNumber: number, options?: IDBPagingCommandOptions): Promise<T[]>;
+    next(pages?: number, options?: boolean | IDBPagingCommandOptions): Promise<T[]>;
+    prev(pages?: number, options?: boolean | IDBPagingCommandOptions): Promise<T[]>;
+    first(options?: IDBPagingIndexOptions): Promise<T[]>;
+    last(options?: IDBPagingIndexOptions): Promise<T[]>;
+    count(options?: IDBPagingIndexOptions): Promise<number>;
 }
 export {};
